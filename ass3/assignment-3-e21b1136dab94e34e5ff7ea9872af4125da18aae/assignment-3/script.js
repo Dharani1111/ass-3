f3ce57606ac5843d0bd5ad688ddd8b6a35f84f3f
Dharani1111 <87258531+Dharani1111@users.noreply.github.com>
2021-07-12 09:20:11 +0530

    Add files via upload

---
+++ ass3/assignment-3-e21b1136dab94e34e5ff7ea9872af4125da18aae/assignment-3/script.js
@@ -0,0 +1,8 @@
+document.getElementById("name").value;
+
+function myfunc(){
+    var txt = document.getElementById("name").value;
+ 
+    document.getElementById("demo").innerHTML="hello "+ txt +" welocome to our page"
+
+ }
